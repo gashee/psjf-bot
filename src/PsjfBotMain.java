2c537c332d0633e759d89fde3f93711806fc8551
DownToSky <mme94@scarletmail.rutgers.edu>
2018-06-16 12:59:01 -0400

    now works better for testing

--- src/PsjfBotMain.java
+++ src/PsjfBotMain.java
@@ -1,19 +1,72 @@
-import java.io.File;
 import me.philippheuer.twitch4j.*;
+import me.philippheuer.twitch4j.endpoints.ChannelEndpoint;
+import me.philippheuer.twitch4j.endpoints.TMIEndpoint;
+import me.philippheuer.twitch4j.model.tmi.Chatter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class PsjfBotMain {
 
-    public static String game = "League of Legends";
-
+	
+	
+    private static void load_creds() {
+    	
+    	
+    	return;
+    }
+    
     public static void main(String[] args) throws Exception {
+    	String client_id = "";
+    	String client_secret = "";
+    	String oauth = "";
+    	String channel = "thegashee";
+    	
+    	String cred_path = "src/creds.json";
+    	JSONParser parser = new JSONParser();
+    	try
+        {
+            Object object = parser
+                    .parse(new FileReader(cred_path));
+            
+            JSONObject jsonObject = (JSONObject)object;
+            
+            client_id = (String) jsonObject.get("client_id");
+            client_secret = (String) jsonObject.get("client_secret");
+            oauth = (String) jsonObject.get("oauth");
+            
+            TwitchClient twitchClient = TwitchClientBuilder.init()
+        			.withClientId(client_id)
+        			.withClientSecret(client_secret)
+        			.withAutoSaveConfiguration(true)
+        			.withConfigurationDirectory(new File("config"))
+        			.withCredential(oauth)
+        			.connect();
+            Long channelId = twitchClient.getUserEndpoint().getUserIdByUserName(channel).get();
+            ChannelEndpoint channelEndpoint = twitchClient.getChannelEndpoint(channelId);
+            channelEndpoint.registerEventListener();
+            
+            TMIEndpoint tmiEndpoint = twitchClient.getTMIEndpoint();
+            Chatter chatter = tmiEndpoint.getChatters(channel);
+            for(String user : chatter.getViewers()) {
+            	System.out.println("Viewer " + user + " is currently present!");
+            }
+        }catch(FileNotFoundException fe)
+        {
+            fe.printStackTrace();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
 
-    	TwitchClient twitchClient = TwitchClientBuilder.init()
-    			.withClientId("eebzjumiflb9g1ygt68mj44l3oosaw")
-    			.withClientSecret("s4igwragwskxtns5n00n2b9z0eac9o")
-    			.withAutoSaveConfiguration(true)
-    			.withConfigurationDirectory(new File("config"))
-    			.withCredential("oauth:6ofq7eruad2c8nxkic3cyfj8licka5") // Get your token at: https://twitchapps.com/tmi/
-    			.connect();
+    	
+    	
     }
     
     
